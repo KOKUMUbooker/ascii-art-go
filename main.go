0c6d8cd6d62f8b05048c27d9663ae5f07026d3c1
fian <frihkian05@gmail.com>
2026-01-26 15:33:08 +0300

    itterating through the file and saving it as an arrray

--- main.go
+++ main.go
@@ -1,4 +1,40 @@
 package main
 
+import ( 
+"os"
+"fmt"
+"strings"
+)
+
+// func main() {
+// 	content, _:= os.ReadFile("standard.txt")
+// 	newcontent := string(content)
+
+// 	// char := strings.Split(newcontent, "\n\n")
+// 	// store := []string{}
+// 	// for _, c := range char {
+// 	// 	if c == "" {
+// 	// 		continue
+// 	// 	}
+// 	// 	store = append(store, c)
+// 	// }
+
+// 	// fmt.Println(store[0])
+	
+// }
+
 func main() {
+	content, _ := os.ReadFile("standard.txt")
+	newcontent := string(content)
+
+	char := strings.Split(newcontent, "\n\n")
+	store := []string{}
+	
+	for _, c := range char {
+		trimmed := strings.TrimSpace(c)
+		if trimmed != "" {
+			store = append(store, c)
+		}
+	}
+	fmt.Print(store[4])
 }
