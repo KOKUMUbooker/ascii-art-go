3b6b87541d231c06bf8ed1692ae01a27e6af90f8
Michelle Kang'ethe <mkangeth@noreply.learn.zone01kisumu.ke>
2026-01-26 12:48:37 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
+
+func getInput() string {
+	if len(os.Args) < 2 {
+		return ""
+	}
+	return os.Args[1]
 }
+
+func main() {
+	input := getInput()
+
+	if input == "" {
+		return
+	}
+
+	fmt.Println(input)
+}
+
